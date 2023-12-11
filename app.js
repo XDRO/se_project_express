65ca77031a078d962ab6065fddc9d24c2407c6af
XDRO <joshuaarcher76@gmail.com>
2023-12-11 10:12:41 -0600

    change mongoose connect to then/catch block

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect("mongodb://localhost:27017/wtwr_db", (r) => {
-  console.log("connected to DB", r);
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((e) => console.error(e));
 
 app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening on
   console.log(`App listening at port ${PORT}`);
 });
+
+// run in gitbash to start database "C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath="c:\data"
