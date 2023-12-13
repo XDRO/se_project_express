31b3e2a95c945144d4b56e3f92eac35712f7c8c0
XDRO <joshuaarcher76@gmail.com>
2023-12-13 14:50:24 -0600

    commit for studdy buddy review

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { createUser, getUsers, getUser } = require("../controllers/user");
 
 // Create
-router.post("/users", createUser);
+router.post("/", createUser);
 // read
 router.get("/users", getUsers);
 
