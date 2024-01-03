7d642babd03512bdac59f407a047fd39d523b562
XDRO <joshuaarcher76@gmail.com>
2024-01-03 15:26:33 -0600

    add app.post /signin and signup to app.js, remove roter.post get and get from user.js routes

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
+
 const app = express();
+
 const globalErrorHandler = require("./controllers/errorController");
+
+const { createUser, login } = require("./controllers/user");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -23,6 +27,9 @@ app.use((req, res, next) => {
 });
 
 app.use(routes);
+
+app.post("signin", login);
+app.post("signup", createUser);
 
 app.use(globalErrorHandler);
 
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,3 @@
 const router = require("express").Router();
 
-const {
-  createUser,
-  getUsers,
-  getUserById,
-  login,
-} = require("../controllers/user");
-
-router.post("/", createUser);
-
-router.post("/signin", login);
-
-router.get("/", getUsers);
-
-router.get("/:userId", getUserById);
-
 module.exports = router;
