b848b162ff64b930a9c5254b5b1905ccd19a15da
XDRO <joshuaarcher76@gmail.com>
2024-01-03 13:55:34 -0600

    create error for duplicateKeys in createUser

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,10 @@
 const { default: mongoose } = require("mongoose");
 const user = require("../models/user");
-const { HTTP_BAD_REQUEST, HTTP_NOT_FOUND } = require("../utils/error");
+const {
+  HTTP_BAD_REQUEST,
+  HTTP_NOT_FOUND,
+  MONGO_DB_DUPLICATE_ERROR,
+} = require("../utils/error");
 
 const createUser = (req, res, next) => {
   // 2nd task is to update this controller
@@ -8,9 +12,9 @@ const createUser = (req, res, next) => {
   // all data must be contained in req body, possibly in the .create and .then methods or maybe create a header, payload, and signature
   // line above is speculation
   // check if there is not an already an exsiting user with a matching email to the one in the req body
-  const { name, avatar } = req.body;
+  const { name, avatar, email, password } = req.body;
   user
-    .create({ name, avatar })
+    .create({ name, avatar, email, password })
     .then((item) => {
       res.send({ data: item });
     })
@@ -19,8 +23,13 @@ const createUser = (req, res, next) => {
         const validationError = new Error(e.message);
         validationError.statusCode = HTTP_BAD_REQUEST;
         next(validationError);
+      } else if (e.statusCode === 11000) {
+        const duplicateKeyError = new Error(e.message);
+        duplicateKeyError.statusCode = MONGO_DB_DUPLICATE_ERROR;
+        throw duplicateKeyError;
+      } else {
+        next(e);
       }
-      next(e);
     });
 };
 
