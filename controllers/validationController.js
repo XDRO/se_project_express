86276c11f304732c15799b0d7d02a2560925fd77
XDRO <joshuaarcher76@gmail.com>
2024-01-09 11:07:04 -0500

    name and email validation inside of validation controller is working

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -1,44 +1,49 @@
-const user = require("../models/user");
 const { HTTP_BAD_REQUEST, HTTP_CONFLICT } = require("../utils/error");
+const user = require("../models/user");
+const { default: isEmail } = require("validator/lib/isEmail");
 
 module.exports.userValidation = async (req, res, next) => {
   try {
     const { name, avatar, email } = req.body;
 
     if (!name || name.length < 2 || name.length > 30) {
-      const error = new Error();
+      const error = new Error("Name is an incorrect length or field is empty");
       error.statusCode = HTTP_BAD_REQUEST;
-      error.message = "name is required";
-      return next(error);
+      throw error;
     }
-    const isValidUrl = (url) => {
-      try {
-        new URL(url);
-        return true;
-      } catch (error) {
-        return false;
-      }
-    };
-    if (!avatar || isValidUrl(avatar)) {
-      const error = new Error();
+    if (!email || !isEmail(email)) {
+      console.log({ email });
+      const error = new Error("Not a valid email address");
       error.statusCode = HTTP_BAD_REQUEST;
-      error.message = "avatar is required";
-      return next(error);
-    }
-    if (!email) {
-      const error = new Error();
-      error.statusCode = HTTP_BAD_REQUEST;
-      error.message = "Not a valid email";
-      return next(error);
+      throw error;
     }
     const existingUser = await user.findOne({ email });
     if (existingUser) {
-      const duplicateError = new Error({ message: "Email already in use" });
-      duplicateError.statusCode = HTTP_CONFLICT;
-      throw duplicateError;
+      console.log({ email });
+      const error = new Error("Email already in use");
+      error.statusCode = HTTP_CONFLICT;
+      throw error;
     }
   } catch (e) {
     return next(e);
   }
   return next();
 };
+
+// const isValidUrl = (url) => {
+//   try {
+//     new URL(url);
+//     return true;
+//   } catch (error) {
+//     return false;
+//   }
+// };
+// console.log({ avatar });
+// if (!avatar || isValidUrl(avatar)) {
+//   console.log({ avatar });
+//   const error = new Error();
+//   error.statusCode = HTTP_BAD_REQUEST;
+//   error.message = "avatar is required";
+//   return next(error);
+// }
+// console.log({ email });
