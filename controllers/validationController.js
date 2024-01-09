7d25d8bff2656951d801b65f20a7d3c6ec96a006
XDRO <joshuaarcher76@gmail.com>
2024-01-09 11:10:26 -0500

    get avatar validation working

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -1,6 +1,7 @@
 const { HTTP_BAD_REQUEST, HTTP_CONFLICT } = require("../utils/error");
 const user = require("../models/user");
 const { default: isEmail } = require("validator/lib/isEmail");
+const { default: isURL } = require("validator/lib/isURL");
 
 module.exports.userValidation = async (req, res, next) => {
   try {
@@ -24,26 +25,13 @@ module.exports.userValidation = async (req, res, next) => {
       error.statusCode = HTTP_CONFLICT;
       throw error;
     }
+    if (!avatar || !isURL(avatar)) {
+      const error = new Error("Avatar URL is invalid");
+      error.statusCode = HTTP_BAD_REQUEST;
+      throw error;
+    }
   } catch (e) {
     return next(e);
   }
   return next();
 };
-
-// const isValidUrl = (url) => {
-//   try {
-//     new URL(url);
-//     return true;
-//   } catch (error) {
-//     return false;
-//   }
-// };
-// console.log({ avatar });
-// if (!avatar || isValidUrl(avatar)) {
-//   console.log({ avatar });
-//   const error = new Error();
-//   error.statusCode = HTTP_BAD_REQUEST;
-//   error.message = "avatar is required";
-//   return next(error);
-// }
-// console.log({ email });
