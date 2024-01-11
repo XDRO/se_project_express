8763b3c4b0a30f5952e0899e5c37b31a65c35e14
XDRO <joshuaarcher76@gmail.com>
2024-01-11 17:43:36 -0500

    get delete item working, by using code from stack overflow, and passing in the route in which to get the itemId, req.params.itemId

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -48,86 +48,26 @@ module.exports.getItems = (req, res, next) => {
     });
 };
 
-// module.exports.deleteItem = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const item = await clothingItems.findById(id).orFail();
-//     console.log({ item });
-
-//     if (!item.owner.equals(req.user._id)) {
-//       return res.status(403).send("Permission not granted");
-//     }
-
-//     await clothingItems.deleteOne(item);
-
-//     res.status(200).send("Item deleted successfully");
-//   } catch (e) {
-//     next(e);
-//   }
-// };
-
-module.exports.deleteItem = (req, res, next) => {
-  // Pull the ID from the params
-  const { id } = req.params;
-
-  // Search for the item by ID
-  clothingItems
-    .findById(id)
-    .orFail() // if no such item is found, an error will be thrown
-    .then((item) => {
-      // here is where you need to compare the item's owner to the current user's ID
-      // item.owner already is an ObjectID. req.user._id is a string, and that's fine
-      if (!item.owner.equals(req.user._id)) {
-        // forbidden
-        return res.status(403).json({ message: "forbidden" });
-      } else {
-        // if not forbidden, then we can delete the request
-        clothingItems.deleteOne(item).then((item) => {
-          return res.status(HTTP_OK_REQUEST).send({ item });
+module.exports.deleteItem = (req, res) => {
+  const owner = req.params.itemId; // add something to this
+  console.log({ owner });
+  const reqUser = req.user._id;
+  clothingItems.findById({ _id: owner }).then((item) => {
+    if (item.userId != reqUser) {
+      return res.status(403).json({ message: "not authorized" });
+    } else {
+      clothingItems
+        .deleteOne({ _id: owner, userId: reqUser })
+        .then(() => {
+          return res.status(HTTP_OK_REQUEST).json({ message: "item deleted" });
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json({ message: "delete item unsuccessful" });
         });
-      }
-    })
-    .catch((e) => {
-      // handle errors
-      if (e instanceof mongoose.CastError) {
-        const castError = new Error(e.message);
-        castError.statusCode = HTTP_BAD_REQUEST;
-        next(castError);
-      } else {
-        next(e);
-      }
-    });
+    }
+  });
 };
-
-// module.exports.deleteItem = (req, res, next) => {
-//   const userId = req.user._id;
-
-//   const userIdObjectId = mongoose.Types.ObjectId(userId);
-
-//   clothingItems
-//     .find({ _id: itemId, owner: userIdObjectId })
-//     // .findByIdAndDelete(itemId)
-//     .orFail()
-//     .then((item) => {
-//       // if (userIdObjectId != userId) {
-//       //   return res.status(403).json({ message: "forbidden" });
-//       // }
-//       res.status(HTTP_OK_REQUEST).send({ item });
-//     })
-//     .catch((e) => {
-//       if (e instanceof mongoose.CastError) {
-//         const castError = new Error(e.message);
-//         castError.statusCode = HTTP_BAD_REQUEST;
-//         next(castError);
-//       } else if (e instanceof mongoose.Error.DocumentNotFoundError) {
-//         const notFoundError = new Error(e.message);
-//         notFoundError.statusCode = HTTP_NOT_FOUND;
-//         next(notFoundError);
-//       } else {
-//         next(e);
-//       }
-//     });
-// };
 
 module.exports.likeItem = (req, res, next) => {
   const itemId = req.params.itemId;
--- test.test.js
+++ test.test.js
@@ -262,29 +262,128 @@ app.use("/users/me", auth, require("./routes/index"), (req, res, next) => {
 // } catch {
 //   console.log(error);
 // }
+const mongoose = require("mongoose");
 
-const deleteClothingItem = async (req, res) => {
-  try {
-    // Pull the ID from the params
-    const { id } = req.params;
+const productSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    userId: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    productImage: { type: String, required: false },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    gender: { type: String, required: true },
+  },
+  { timestamps: { createdAt: "created_at" } },
+);
 
-    // Search for the item by ID
-    const item = await clothingItems.findById(id).orFail();
+module.exports = mongoose.model("Product", productSchema);
 
-    // Compare the item's owner to the current user's ID
-    if (!item.owner.equals(req.user._id)) {
-      // Forbidden
-      return res.status(403).send("Forbidden");
+const deleteProduct = (req, res) => {
+  const id = req.params.productId;
+  Product.findById({ _id: id }).then((product) => {
+    if (product.userId != req.user._id) {
+      return res.status(401).json("Not authorized");
+    } else {
+      Product.deleteOne({ _id: id })
+        .exec()
+        .then(() => {
+          return res.status(200).json({
+            message: "Product deleted succesfully",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json({
+            error: err,
+          });
+        });
     }
-
-    // If not forbidden, delete the item
-    await ClothingItem.deleteOne(item);
-
-    // Handle success response
-    res.status(200).send("Item deleted successfully");
-  } catch (err) {
-    // Handle errors
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
+  });
 };
+// tutor code below
+module.exports.deleteItem = (req, res, next) => {
+  // Pull the ID from the params
+  const id = req.params;
+
+  // Search for the item by ID
+  clothingItems
+    .findById({ _id: id })
+    .orFail() // if no such item is found, an error will be thrown
+    .then((item) => {
+      // here is where you need to compare the item's owner to the current user's ID
+      // item.owner already is an ObjectID. req.user._id is a string, and that's fine
+      if (!item.owner.equals(req.user._id)) {
+        // forbidden
+        return res.status(403).json({ message: "forbidden" });
+      } else {
+        // if not forbidden, then we can delete the request
+        clothingItems.deleteOne(item).then((item) => {
+          return res.status(HTTP_OK_REQUEST).send({ item });
+        });
+      }
+    })
+    .catch((e) => {
+      // handle errors
+      if (e instanceof mongoose.CastError) {
+        const castError = new Error(e.message);
+        castError.statusCode = HTTP_BAD_REQUEST;
+        next(castError);
+      } else {
+        next(e);
+      }
+    });
+};
+
+// old code
+// module.exports.deleteItem = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const item = await clothingItems.findById(id).orFail();
+//     console.log({ item });
+
+//     if (!item.owner.equals(req.user._id)) {
+//       return res.status(403).send("Permission not granted");
+//     }
+
+//     await clothingItems.deleteOne(item);
+
+//     res.status(200).send("Item deleted successfully");
+//   } catch (e) {
+//     next(e);
+//   }
+// };
+
+// module.exports.deleteItem = (req, res, next) => {
+//   const userId = req.user._id;
+
+//   const userIdObjectId = mongoose.Types.ObjectId(userId);
+
+//   clothingItems
+//     .find({ _id: itemId, owner: userIdObjectId })
+//     // .findByIdAndDelete(itemId)
+//     .orFail()
+//     .then((item) => {
+//       // if (userIdObjectId != userId) {
+//       //   return res.status(403).json({ message: "forbidden" });
+//       // }
+//       res.status(HTTP_OK_REQUEST).send({ item });
+//     })
+//     .catch((e) => {
+//       if (e instanceof mongoose.CastError) {
+//         const castError = new Error(e.message);
+//         castError.statusCode = HTTP_BAD_REQUEST;
+//         next(castError);
+//       } else if (e instanceof mongoose.Error.DocumentNotFoundError) {
+//         const notFoundError = new Error(e.message);
+//         notFoundError.statusCode = HTTP_NOT_FOUND;
+//         next(notFoundError);
+//       } else {
+//         next(e);
+//       }
+//     });
+// };
