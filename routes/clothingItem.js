c5e0133eb5749ceef21c7ed9e9bd3240c36b93c0
XDRO <joshuaarcher76@gmail.com>
2024-01-12 17:48:44 -0500

    remove clothingItem comments

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -26,14 +26,3 @@ router.delete("/:itemId", auth, deleteItem);
 router.delete("/:itemId/likes", auth, dislikeItem);
 
 module.exports = router;
-
-// Routes for clothing items:
-// GET /items to return all items from the database
-
-// POST /items to create an item with name , imageUrl , and weather passed in the
-// request body. owner should be set from the req.user._id
-// Project 13. Checklist 4
-// DELETE /items/:id to delete an item by _id . The user can’t delete a card that
-// they didn’t create
-// PUT /items/:id/likes to like an item ( new: true is passed to options)
-// DELETE /items/:id/likes to unlike an item ( new: true is passed to options)
