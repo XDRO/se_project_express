94b1e74ae98729be4dc83af6f631b0ad9b201d6a
XDRO <joshuaarcher76@gmail.com>
2024-01-12 17:47:36 -0500

    change global error handler to async await, return undefined

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,9 +1,10 @@
 const { HTTP_INTERNAL_SERVER_ERROR } = require("../utils/error");
 
-module.exports.globalErrorHandler = (error, req, res, next) => {
-  // console.error(error);
+module.exports.globalErrorHandler = async (error, req, res, next) => {
   res.status(error.statusCode || HTTP_INTERNAL_SERVER_ERROR).json({
     message: error.message,
   });
   next();
+  // Return undefined to satisfy eslint
+  return undefined;
 };
