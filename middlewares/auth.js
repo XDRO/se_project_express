4d446de17facdecf2e9b80a0a9d09cdf746ee29a
XDRO <joshuaarcher76@gmail.com>
2024-01-12 17:06:23 -0500

    change const handleAuthError into an arrow function without {}

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,9 +2,10 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const { HTTP_UNAUTHORIZED } = require("../utils/error");
 
-const handleAuthError = (res) => {
-  return res.status(HTTP_UNAUTHORIZED).send({ message: "Authorization Error" });
-};
+const handleAuthError = (res, message) =>
+  res
+    .status(HTTP_UNAUTHORIZED)
+    .send({ message: message || "Authorization Error" });
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
--- test.test.js
+++ test.test.js
@@ -540,3 +540,28 @@
 //       }
 //     });
 // };
+
+// const handleAuthError = (res) => {
+//   return res
+//   .status(HTTP_UNAUTHORIZED)
+//   .send({ message: "Authorization Error" });
+// };
+
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+
+//   if (!authorization || !authorization.startsWith("Bearer ")) {
+//     return handleAuthError(res);
+//   }
+
+//   const token = authorization.replace("Bearer ", "");
+
+//   try {
+//     const payload = jwt.verify(token, JWT_SECRET);
+//     req.user = payload;
+//   } catch {
+//     return handleAuthError(res, "Invalid token");
+//   }
+//   next();
+//   return module.exports;
+// };
