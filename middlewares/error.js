0ce846063342de83d2974244e7d6681ea05f8c6d
XDRO <joshuaarcher76@gmail.com>
2024-01-13 13:20:30 -0500

    add return to next() in error.js middleware

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,8 +4,5 @@ module.exports.globalErrorHandler = async (error, req, res, next) => {
   res.status(error.statusCode || HTTP_INTERNAL_SERVER_ERROR).json({
     message: error.message,
   });
-  next();
-  // Return undefined to satisfy eslint
-  // could be improved
-  return undefined;
+  return next();
 };
