394df30018c8de21cb6e3f3bd4cb3cb09b6ee4a1
XDRO <joshuaarcher76@gmail.com>
2024-02-21 15:49:06 -0600

    reconfigure error.js to classes that extend to error class

--- utils/error.js
+++ utils/error.js
@@ -1,10 +1,59 @@
+class HTTP_BAD_REQUEST extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+class HTTP_FORBIDDEN extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 403;
+  }
+}
+
+class HTTP_NOT_FOUND extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+  }
+}
+
+class HTTP_INTERNAL_SERVER_ERROR extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 500;
+  }
+}
+
+class HTTP_UNAUTHORIZED extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+  }
+}
+
+class HTTP_CONFLICT extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 409;
+  }
+}
+
+class MONGO_DB_DUPLICATE_ERROR extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 11000;
+  }
+}
+
 module.exports = {
   HTTP_OK_REQUEST: 200,
-  HTTP_BAD_REQUEST: 400,
-  HTTP_FORBIDDEN: 403,
-  HTTP_NOT_FOUND: 404,
-  HTTP_INTERNAL_SERVER_ERROR: 500,
-  HTTP_UNAUTHORIZED: 401,
-  HTTP_CONFLICT: 409,
-  MONGO_DB_DUPLICATE_ERROR: 11000,
+  HTTP_BAD_REQUEST,
+  HTTP_FORBIDDEN,
+  HTTP_NOT_FOUND,
+  HTTP_INTERNAL_SERVER_ERROR,
+  HTTP_UNAUTHORIZED,
+  HTTP_CONFLICT,
+  MONGO_DB_DUPLICATE_ERROR,
 };
