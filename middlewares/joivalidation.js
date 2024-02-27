50b880f3e085428bcacfb991519a0a933ca6f573
XDRO <joshuaarcher76@gmail.com>
2024-02-27 13:07:00 -0600

    change hot, warm, cold to lowercase

--- middlewares/joivalidation.js
+++ middlewares/joivalidation.js
@@ -20,7 +20,7 @@ module.exports.validateCardBody = celebrate({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
-    weather: Joi.string().valid("Hot", "Warm", "Cold").required(),
+    weather: Joi.string().valid("hot", "warm", "cold").required(),
   }),
 });
 
